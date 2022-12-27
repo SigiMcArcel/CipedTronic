668777a781ff3a1ac9d7611785f3823f716ccfb4
Marcel Siegwart <marcel.siegwart@gmx.ch>
2022-12-27 11:21:54 +0100

    Better protocoll implemented Clean up Not tested too high memory usage. has to be improved

--- CipedTronicMcu/CipedTronicMCU/main.c
+++ CipedTronicMcu/CipedTronicMCU/main.c
@@ -9,15 +9,24 @@
 #include "Platform/Timer.h"
 #include "Platform/GPIO.h"
 #include "Platform/Comparator.h"
-#include "Protocoll.h"
+#include "CipedTronic/Protocol.h"
 #include "Platform/Counter.h"
 #include "Platform/USBSerial.h"
 #include "CipedTronic/BLE.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define CIPED_REGISTER_PULSES 30
+#define CIPED_REGISTER_PULSES_PER_SECOND 31
+#define CIPED_REGISTER_PULSES_PER_SECOND_MAX 31
+#define CIPED_REGISTER_PULSES_PER_SECOND_AVERAGE 32
+
+
 static volatile uint32_t _LastTimerTick = 0;
 static volatile uint32_t _Delay = 0;
+
+
 
 int main (void)
 {
@@ -51,6 +60,10 @@ int main (void)
 		uint32_t tick = TimerGetTick();
 		uint32_t diff = tick - _LastTimerTick;
 		CounterHandler();
+		ProtocolRegisters[CIPED_REGISTER_PULSES].Data = CounterGetCounter();
+		ProtocolRegisters[CIPED_REGISTER_PULSES_PER_SECOND].Data = CounterGetCounterPerSecond();
+		ProtocolRegisters[CIPED_REGISTER_PULSES_PER_SECOND_MAX].Data = CounterGetMaxCounterPerSecond();
+		ProtocolRegisters[CIPED_REGISTER_PULSES_PER_SECOND_AVERAGE].Data = CounterGetAvgCounterPerSecond();
 		
 		ProtocolHandler();
 		BLEProcess();
